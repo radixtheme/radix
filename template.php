77c011fb6932a2a39cdb92744e66b98697acc289
Arshad <arshad@chummun.com>
2012-09-08 18:20:29 +0400

    template for panels pane

--- template.php
+++ template.php
@@ -4,11 +4,11 @@
  * Implements template_preprocess_page().
  */
 function radix_preprocess_page(&$variables) {
-  // Search Form
-  if (module_exists('search')) {
-    $search_form = drupal_get_form('search_block_form');
-    $search_form['#attributes']['class'][] = 'search-form';
-    $search_form['search_block_form']['#attributes']['placeholder'] = t('Search');
-    $variables['search_form'] = render($search_form);
-  }
+}
+
+/**
+ * Implements template_preprocess_panels_pane().
+ */
+function radix_preprocess_panels_pane(&$variables) {
+  dpm($variables);
 }
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -66,70 +66,57 @@
  * @see html.tpl.php
  */
 ?>
+<div id="page-wrapper" class="container container-fluid">
 
-  <div id="page-wrapper"><div id="page">
-
-    <div id="header"><div class="section clearfix">
-
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name"><strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong></div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </h1>
-            <?php endif; ?>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
-      <?php endif; ?>
-      <?php if ($search_form) : print $search_form; endif; ?>
-    </div></div> <!-- /.section, /#header -->
-
-    <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t(''))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t(''))); ?>
-      </div></div> <!-- /.section, /#navigation -->
+  <div id="header"><div class="row-fluid clearfix">
+    <?php if ($site_name || $site_slogan): ?>
+      <div id="name-and-slogan" class="clearfix span4">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php endif; ?>
+        <?php if ($site_name): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="brand"><span><?php print $site_name; ?></span></a>        
+        <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <p class="slogan"><?php print $site_slogan; ?></p>
+        <?php endif; ?>
+      </div> <!-- /#name-and-slogan -->
     <?php endif; ?>
+    <?php if ($main_menu): ?>
+      <div id="navigation" class="span8">
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'clearfix')), 'heading' => t(''))); ?>  
+      </div> <!-- /#navigation -->
+   <?php endif; ?>
+  </div></div> <!-- /#header -->
 
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
+  <?php if ($breadcrumb): ?>
+    <div id="breadcrumb" class="row-fluid visible-desktop">
+      <div class="span12">
+        <?php print $breadcrumb; ?>
+      </div>
+    </div>
+  <?php endif; ?>
 
-    <?php print $messages; ?>
+  <?php print $messages; ?>
 
-    <div id="main-wrapper"><div id="main" class="clearfix">
+  <div id="main-wrapper">
+    <div id="content" class="row-fluid">
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div> <!-- /#content -->
+  </div> <!-- /#main-wrapper -->
 
-      <div id="content" class="column"><div class="section">
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-      </div></div> <!-- /.section, /#content -->
+  <div id="footer"><div class="section">
+    <?php print render($page['footer']); ?>
+  </div></div> <!-- /.section, /#footer -->
 
-    </div></div> <!-- /#main, /#main-wrapper -->
-
-    <div id="footer"><div class="section">
-      <?php print render($page['footer']); ?>
-    </div></div> <!-- /.section, /#footer -->
-
-  </div></div> <!-- /#page, /#page-wrapper -->
+</div> <!-- /#page-wrapper -->
