708f43ced2257023ff6406e469e65d75273e6cfa
Arshad <arshad@chummun.com>
2012-09-08 18:21:09 +0400

    remove template_preprocess_panels_pane

--- template.php
+++ template.php
@@ -4,11 +4,5 @@
  * Implements template_preprocess_page().
  */
 function radix_preprocess_page(&$variables) {
-}
-
-/**
- * Implements template_preprocess_panels_pane().
- */
-function radix_preprocess_panels_pane(&$variables) {
-  dpm($variables);
+  //
 }
