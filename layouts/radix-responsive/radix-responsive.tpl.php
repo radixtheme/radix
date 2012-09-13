6f8a30d54702d28ba58460e904168d02afbc3498
Arshad <arshad@chummun.com>
2012-09-13 22:27:06 +0400

    radix reponsive panels layout

---
+++ layouts/radix-responsive/radix-responsive.tpl.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @file
+ * Template for Radix Responsive.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+
+<div class="panel-display clearfix container <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+
+  <div class="span12 panel-panel">
+    <div class="panel-panel-inner">
+      <?php print $content['header']; ?>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="span3 panel-panel">
+      <div class="panel-panel-inner">
+        <?php print $content['left']; ?>
+      </div>
+    </div>
+    <div class="span6 panel-panel">
+      <div class="panel-panel-inner">
+        <?php print $content['content']; ?>
+      </div>
+    </div>
+    <div class="span3 panel-panel">
+      <div class="panel-panel-inner">
+        <?php print $content['right']; ?>
+      </div>
+    </div>
+  </div>
+
+  <div class="span12 panel-panel">
+    <div class="panel-panel-inner">
+      <?php print $content['footer']; ?>
+    </div>
+  </div>
+  
+</div><!-- /.panel-display -->
