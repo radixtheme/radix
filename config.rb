2ee35f06e20925cfe39fba6764f77d315498657c
Arshad Chummun <arshad@chummun.com>
2013-02-12 00:54:28 +0400

    Updated config.rb for radix and radix_starter

--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@
 require "compass_twitter_bootstrap";
 require "compass_radix";
 
+environment = :production
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/stylesheets"
@@ -12,10 +14,10 @@ javascripts_dir = "assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :nested
+output_style = (environment == :development) ? :expanded : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+line_comments = (environment == :development) ? true : false
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
