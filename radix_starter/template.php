577f720fbe90971904c41f591cbe745c8d913a09
Arshad Chummun <arshad@chummun.com>
2013-05-07 20:31:26 +0400

    Add sample template includes files in radix_starter

--- radix_starter/template.php
+++ radix_starter/template.php
@@ -1,6 +1,12 @@
 <?php
-
 /**
  * @file
  * Theme functions
  */
+
+$theme_path = drupal_get_path('theme', 'radix_starter');
+require_once $theme_path . '/includes/structure.inc';
+require_once $theme_path . '/includes/form.inc';
+require_once $theme_path . '/includes/menu.inc';
+require_once $theme_path . '/includes/comment.inc';
+require_once $theme_path . '/includes/node.inc';
