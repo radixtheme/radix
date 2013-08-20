fe7063d2728977c6858cce0d1428a70be8afeba6
Arshad Chummun <arshad@chummun.com>
2013-08-20 21:50:32 +0400

    Issue [#2065135] by Andrew Edwards: removed duplicated radix styles from subtheme

--- radix_starter/template.php
+++ radix_starter/template.php
@@ -10,3 +10,16 @@ require_once $theme_path . '/includes/form.inc';
 require_once $theme_path . '/includes/menu.inc';
 require_once $theme_path . '/includes/comment.inc';
 require_once $theme_path . '/includes/node.inc';
+
+/**
+ * Implements hook_css_alter().
+ */
+function radix_starter_css_alter(&$css) {
+  $radix_path = drupal_get_path('theme', 'radix');
+
+  // Radix now includes compiled stylesheets for demo purposes.
+  // We remove these from our subtheme since they are already included 
+  // in compass_radix.
+  unset($css[$radix_path . '/assets/stylesheets/radix-style.css']);
+  unset($css[$radix_path . '/assets/stylesheets/radix-print.css']);
+}
