f3dc406a9acccc2d60cbeb7089035816ee92d769
Arshad Chummun <arshad@chummun.com>
2013-09-04 15:44:12 +0400

    Removed navbar toggle implementation from custom js

--- assets/javascripts/radix-script.js
+++ assets/javascripts/radix-script.js
@@ -12,24 +12,6 @@
       $(this).removeClass('open');
     });
 
-    // navbar collapse
-    $('.btn-navbar').click(function() {
-      var target = $($(this).data().target);
-      if (target.hasClass('in')) {
-        target.animate({
-          height: 0
-        }, 500, function() {
-          $(this).removeClass('in');
-        });
-      } else {
-        target.animate({
-          height: target.find('.inner').height()
-        }, 500, function() {
-          $(this).addClass('in');
-        });
-      }
-    });
-
     // Tooltip.
     $('[data-toggle="tooltip"]').tooltip();
   });
