8d953791b6744d03ac01835ee106650ef25c1a83
Arshad Chummun <arshad@chummun.com>
2013-09-10 17:42:19 +0400

    Added an html5 kit

---
+++ kits/html5/templates/system/page.tpl.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ * @see html.tpl.php
+ */
+?>
+<header id="header" class="header">
+  <div class="container">
+    <div id="navigation" class="navbar">
+      <div class="navbar-inner">
+        <div class="container clearfix">
+          <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+          <a class="btn btn-navbar btn-navbar-menu" data-toggle="collapse" data-target=".nav-menu-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <!-- .btn-navbar-search for collapsed search form -->
+          <?php if ($search_form): ?>
+            <a class="btn btn-navbar btn-navbar-search" data-toggle="collapse" data-target=".nav-search-collapse">
+              <span class="icon-search"></span>
+            </a>
+          <?php endif; ?>
+
+          <?php if ($logo): ?>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo" class="pull-left brand">
+              <?php print $site_name; ?>
+            </a>
+          <?php endif; ?>
+
+          <div class="nav-collapse nav-menu-collapse">
+            <div class="inner">
+              <?php if ($main_menu): ?>
+                <nav id="main-menu" class="main-menu pull-left" role="navigation">
+                  <?php print render($main_menu); ?>
+                </nav> <!-- /#main-menu -->
+              <?php endif; ?>
+            </div>
+          </div>
+
+          <div class="nav-collapse nav-search-collapse">
+            <div class="inner">
+              <?php if ($search_form): ?>
+                <?php print $search_form; ?>
+              <?php endif; ?>
+            </div>
+          </div>
+
+      </div>
+    </div> 
+  </div> <!-- /#navigation -->
+</header>
+
+<main id="main-wrapper" role="main">
+  <div id="main" class="main <?php print (!$is_panel) ? 'container' : ''; ?>">
+    <?php if ($breadcrumb): ?>
+      <div id="breadcrumb" class="visible-desktop">
+        <div class="container">
+          <?php print $breadcrumb; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+    <?php if ($messages): ?>
+      <div id="messages">
+        <div class="container">
+          <?php print $messages; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+    <div id="content">
+      <a id="main-content"></a>
+      <div id="page-header">
+          <div class="container">
+            <?php if ($title): ?>
+              <div class="page-header">
+                <h1 class="title"><?php print $title; ?></h1>
+              </div>
+            <?php endif; ?>
+            <?php if ($tabs): ?>
+              <div class="tabs">
+                <?php print render($tabs); ?>
+              </div>
+            <?php endif; ?>
+            <?php if ($action_links): ?>
+              <ul class="nav nav-pills action-links">
+                <?php print render($action_links); ?>
+              </ul>
+            <?php endif; ?>
+          </div>
+      </div>
+      <?php print render($page['content']); ?>
+    </div>
+  </div>
+</main> <!-- /#main-wrapper -->
+
+<footer id="footer" class="footer">
+  <div class="container">
+    <?php if ($copyright): ?>
+      <small class="copyright pull-left"><?php print $copyright; ?></small>
+    <?php endif; ?>
+    <small class="pull-right"><a href="#"><?php print t('Back to Top'); ?></a></small>
+  </div>
+</footer>
