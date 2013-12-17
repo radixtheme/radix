b25cb82cb5c0b58ecc2edd298e812ead4c25f9c4
naughty_david <naughty_david@782568.no-reply.drupal.org>
2013-12-17 22:02:59 +0400

    Updated doctype for html.tpl.php

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -40,8 +40,7 @@
  * @see template_preprocess_html()
  * @see template_process()
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
