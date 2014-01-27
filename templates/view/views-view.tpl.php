d427a0536dadf19b5675a46c3c55b7e5ee8a9bdb
Arshad <arshad@chummun.com>
2014-01-27 18:27:20 +0400

    Use row instead of row-fluid for view-content

--- templates/view/views-view.tpl.php
+++ templates/view/views-view.tpl.php
@@ -52,7 +52,7 @@
   <?php endif; ?>
 
   <?php if ($rows): ?>
-    <div class="view-content row-fluid">
+    <div class="view-content row">
       <?php print $rows; ?>
     </div>
   <?php elseif ($empty): ?>
