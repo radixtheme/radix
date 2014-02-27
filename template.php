1148ef9a4e172709fcd9d10b15a63f25fb2223f9
Capi Etheriel <barraponto@gmail.com>
2014-02-27 00:50:06 -0300

    Issue #2206229 by barraponto: fixes administer permissions form.

--- template.php
+++ template.php
@@ -12,6 +12,7 @@ require_once dirname(__FILE__) . '/includes/menu.inc';
 require_once dirname(__FILE__) . '/includes/comment.inc';
 require_once dirname(__FILE__) . '/includes/panel.inc';
 require_once dirname(__FILE__) . '/includes/view.inc';
+require_once dirname(__FILE__) . '/includes/admin.inc';
 
 /**
  * Implementation of template_preprocess_html().
