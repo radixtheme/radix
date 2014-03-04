efabb8a0ea804277ed4b5082359628ce8b1f2fc2
Arshad <arshad@chummun.com>
2014-03-04 23:01:41 +0400

    Removed tooltip implementation in js for now

--- assets/javascripts/radix-script.js
+++ assets/javascripts/radix-script.js
@@ -3,11 +3,5 @@
  * JS for Radix.
  */
 (function ($, Drupal, window, document, undefined) {
-
-  Drupal.behaviors.RadixTooltips = {
-    attach: function(context, settings) {
-      $('[data-toggle="tooltip"]', context).tooltip();
-    }
-  };
-  
+  // 
 })(jQuery, Drupal, this, this.document);
