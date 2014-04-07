3c563adb64e4bd4aaab862df740e429d776fda77
mpotter <mpotter@616192.no-reply.drupal.org>
2014-04-07 08:52:38 +0400

    Issue #2227537 by mpotter: Radix getting theme path incorrectly.

--- template.php
+++ template.php
@@ -118,9 +118,10 @@ function radix_preprocess_page(&$variables) {
   $variables['copyright'] = t('Drupal is a registered trademark of Dries Buytaert.');
 
   // Display a message if Sass has not been compiled.
-  $stylesheet_path = path_to_theme() . '/assets/stylesheets/screen.css';
+  $theme_path = drupal_get_path('theme',$GLOBALS['theme']);
+  $stylesheet_path = $theme_path . '/assets/stylesheets/screen.css';
   if (_radix_current_theme() == 'radix') {
-    $stylesheet_path = path_to_theme() . '/assets/stylesheets/radix-style.css';
+    $stylesheet_path = $theme_path . '/assets/stylesheets/radix-style.css';
   }
   if (!file_exists($stylesheet_path)) {
     drupal_set_message(t('It looks like !path has not been created yet. Run !command in your theme directory to create it.', array(
