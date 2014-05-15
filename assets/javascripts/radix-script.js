a2da60a49a9c973fdf4a7b7729dcf92efa39dcb5
Arshad Chummun <arshad@chummun.com>
2014-05-15 20:31:32 +0400

    Show tab if hash is provided via url.

--- assets/javascripts/radix-script.js
+++ assets/javascripts/radix-script.js
@@ -3,5 +3,12 @@
  * JS for Radix.
  */
 (function ($, Drupal, window, document, undefined) {
-  // 
+  $(document).ready(function() {
+    // Show tab if hash is provided via url.
+    if (hash = window.location.hash) {
+      if ($('[href="' + hash + '"]').length) {
+        $('[href="' + hash + '"]').tab('show');
+      }
+    }
+  });
 })(jQuery, Drupal, this, this.document);
