ded9254269aa98b363469e15f2b8fb5282ece9d0
Arshad Chummun <arshad@chummun.com>
2014-05-30 15:40:08 +0400

    Show dropdown on hover

--- assets/javascripts/radix-script.js
+++ assets/javascripts/radix-script.js
@@ -3,5 +3,13 @@
  * JS for Radix.
  */
 (function ($, Drupal, window, document, undefined) {
-  // 
+  $(document).ready(function() {
+    // Show dropdown on hover.
+    $('.dropdown').mouseenter(function() {
+      $(this).addClass('open');
+    });
+    $('.dropdown').mouseleave(function() {
+      $(this).removeClass('open');
+    });
+  });
 })(jQuery, Drupal, this, this.document);
