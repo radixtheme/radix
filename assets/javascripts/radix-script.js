beba021d0631a73f9afcdb6f63c3054e434a12fc
Arshad Chummun <arshad@chummun.com>
2014-05-30 15:43:27 +0400

    Allow main menu dropdown-toggle to be clickable

--- assets/javascripts/radix-script.js
+++ assets/javascripts/radix-script.js
@@ -11,5 +11,11 @@
     $('.dropdown').mouseleave(function() {
       $(this).removeClass('open');
     });
+
+    // Allow main menu dropdown-toggle to be clickable.
+    $('#main-menu .dropdown > a.dropdown-toggle').click(function(e) {
+      e.preventDefault();
+      window.location.href = $(this).attr('href');
+    });
   });
 })(jQuery, Drupal, this, this.document);
