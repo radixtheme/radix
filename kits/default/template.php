375e6e41dfd2453bdc4dc5292015ab0f7e585fbf
Arshad Chummun <arshad@chummun.com>
2015-06-01 23:24:47 +0400

    Added a block.inc file for block hooks

--- kits/default/template.php
+++ kits/default/template.php
@@ -12,6 +12,7 @@ require_once dirname(__FILE__) . '/includes/form.inc';
 require_once dirname(__FILE__) . '/includes/menu.inc';
 require_once dirname(__FILE__) . '/includes/node.inc';
 require_once dirname(__FILE__) . '/includes/panel.inc';
+require_once dirname(__FILE__) . '/includes/block.inc';
 require_once dirname(__FILE__) . '/includes/user.inc';
 require_once dirname(__FILE__) . '/includes/view.inc';
 
