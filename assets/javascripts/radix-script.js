f73ed2b0b01deb29511db3ac623d8613f3bfc5a6
dsnopek <dsnopek@266527.no-reply.drupal.org>
2015-08-18 10:21:30 -0500

    Issue #2500635 by dsnopek: Javascript to make dropdown links clickable causes accessibility issues

--- assets/javascripts/radix-script.js
+++ assets/javascripts/radix-script.js
@@ -3,18 +3,87 @@
  * JS for Radix.
  */
 (function ($, Drupal, window, document, undefined) {
-
-  // Show dropdown on hover.
+  // Allow dropdown links to be clickable by showing dropdowns on hover/focus.
   Drupal.behaviors.radix_dropdown = {
     attach: function(context, setting) {
       $('.dropdown').once('radix-dropdown', function(){
-        // Show dropdown on hover.
-        $(this).mouseenter(function(){
-          $(this).addClass('open');
+        var dropdown = this;
+
+        // Helper function to show the dropdown.
+        function show() {
+          if (!$(dropdown).hasClass('open')) {
+            $('>[data-toggle="dropdown"]', dropdown).trigger('click.bs.dropdown');
+          }
+        }
+
+        // Helper function to hide the dropdown.
+        function hide() {
+          if ($(dropdown).hasClass('open')) {
+            $('>[data-toggle="dropdown"]', dropdown).trigger('click.bs.dropdown');
+          }
+        }
+
+        // Show dropdown on hover and focus.
+        $(this).on('mouseenter.radix.dropdown', function(e) {
+          show();
         });
-        $(this).mouseleave(function(){
-          $(this).removeClass('open');
+        $(this).on('mouseleave.radix.dropdown', function() {
+          hide();
         });
+        $(this).on('focusin.radix.dropdown', function() {
+          show();
+        });
+        $('body').once('radix-dropdown').on('focusout.radix.dropdown', function(e) {
+          var parent = $(e.target).closest('li.radix-dropdown-processed.open').get(0);
+          if (parent) {
+            // Defer to after all handlers so we can see where focus landed.
+            setTimeout(function () {
+              // Don't do anything if no element is focused - that can only
+              // happen with the mouse and this is meant to close the menu
+              // when the keyboard is used to change focus.
+              if (!document.activeElement || document.activeElement === document.body) {
+                return;
+              }
+              // Hide the parent if it doesn't contain the now focused element
+              // and is still open.
+              if (!parent.contains(document.activeElement) && $(parent).hasClass('open')) {
+                $(parent).trigger('click.bs.dropdown');
+              }
+            }, 0);
+          }
+        });
+
+        $(this).on('keydown.radix.dropdown', function(e) {
+          // Prevent up/down arrow from doing anything -- they conflict with
+          // using focus to show the dropdown, and the default Bootstrap keydown
+          // handler will trigger our click handler to visit the link.
+          if (e.keyCode == 38 || e.keyCode == 40) {
+            return false;
+          }
+          // Show/hide dropdown with spacebar.
+          if (e.keyCode == 32) {
+            $('>[data-toggle="dropdown"]', dropdown).trigger('click.bs.dropdown');
+            return false;
+          }
+          // Hide the dropdown with the escape hey.
+          if (e.keyCode == 27) {
+            // Leave focus on the parent after it's hidden.
+            $('>[data-toggle="dropdown"]', dropdown).focus();
+            hide();
+            return false;
+          }
+        });
+
+        // Allow a.dropdown-toggle to be clickable.
+        if ($(this).has('> a.dropdown-toggle')) {
+          $(this).on('click.radix.dropdown', function(e) {
+            var $target = $(e.target);
+            if ($target.parent().get(0) == dropdown && $target.is('a.dropdown-toggle') && $target.attr('href')) {
+              e.preventDefault();
+              window.location.href = $target.attr('href');
+            }
+          });
+        }
       });
     }
   }
@@ -38,14 +107,6 @@
   }
 
   $(document).ready(function() {
-    // Allow main menu dropdown-toggle to be clickable.
-    $('#main-menu .dropdown > a.dropdown-toggle').once('radix-dropdown', function(){
-      $(this).click(function(e) {
-        e.preventDefault();
-        window.location.href = $(this).attr('href');
-      });
-    });
-
     // Show first tab by default.
     // Ignore the "primary" tabs on the node edit page.
     if ($.fn.tab) {
