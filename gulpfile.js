f942ec1b9d47eeeeab8d6e14379fd10b2bec5811
Arshad Chummun <arshad@chummun.com>
2015-11-11 15:56:45 +0400

    Rewrite Radix to use libsass

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,20 @@ var config = require('./config.json');
 // Include Our Plugins
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
-var compass = require('gulp-compass');
 var imagemin = require('gulp-imagemin');
 var pngcrush = require('imagemin-pngcrush');
-var livereload = require('gulp-livereload');
 var shell = require('gulp-shell');
 var gutil = require('gulp-util');
 var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var autoprefix = require('gulp-autoprefixer');
+var bower = require('gulp-bower');
+
+// Bower
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest(config.bowerDir))
+});
 
 // Compress images
 gulp.task('images', function () {
@@ -25,31 +32,36 @@ gulp.task('images', function () {
     .pipe(gulp.dest('assets/images'));
 });
 
+// Compile Our Sass with Bundle[d] Compass
+gulp.task('sass', function() {
+  return gulp.src('assets/sass/*.scss')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        notify.onError({
+          title:    "Gulp",
+          subtitle: "Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+        }) (error);
+        this.emit('end');
+      }}))
+    .pipe(sass({
+      style: 'compressed',
+      errLogToConsole: true,
+      includePaths: config.sassIncludePaths
+    }))
+    .pipe(autoprefix('last 2 versions', '> 1%', 'ie 9', 'ie 10'))
+    .pipe(gulp.dest('assets/stylesheets'));
+});
+
 // Static Server + watching scss files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['bower', 'sass'], function() {
   browserSync.init({
     proxy: config.browserSyncProxy
   })
 
   gulp.watch('assets/sass/**/*.scss', ['sass']);
   gulp.watch('assets/stylesheets/**/*').on('change', browserSync.reload);
-});
-
-// Compile Our Sass with Bundle[d] Compass
-gulp.task('sass', function() {
-  return gulp.src('assets/sass/*.scss')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-      }}))
-    .pipe(compass({
-      config_file: 'config.rb',
-      css: 'assets/stylesheets',
-      sass: 'assets/sass',
-      bundle_exec: true
-    }))
-    .pipe(gulp.dest('assets/stylesheets'));
 });
 
 // Run drush to clear the theme registry.
--- template.php
+++ template.php
@@ -101,7 +101,7 @@ function radix_js_alter(&$javascript) {
   // Add radix-modal when required.
   if (module_exists('ctools')) {
     $ctools_modal = drupal_get_path('module', 'ctools') . '/js/modal.js';
-    $radix_modal = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix-modal.js';
+    $radix_modal = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix.modal.js';
     if (!empty($javascript[$ctools_modal]) && empty($javascript[$radix_modal])) {
       $javascript[$radix_modal] = array_merge(
         drupal_js_defaults(), array('group' => JS_THEME, 'data' => $radix_modal));
@@ -111,7 +111,7 @@ function radix_js_alter(&$javascript) {
   // Add radix-field-slideshow when required.
   if (module_exists('field_slideshow')) {
     $field_slideshow = drupal_get_path('module', 'field_slideshow') . '/field_slideshow.js';
-    $radix_field_slideshow = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix-field-slideshow.js';
+    $radix_field_slideshow = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix.slideshow.js';
     if (!empty($javascript[$field_slideshow]) && empty($javascript[$radix_field_slideshow])) {
       $javascript[$radix_field_slideshow] = array_merge(
         drupal_js_defaults(), array('group' => JS_THEME, 'data' => $radix_field_slideshow));
@@ -120,7 +120,7 @@ function radix_js_alter(&$javascript) {
 
   // Add radix-progress when required.
   $progress = 'misc/progress.js';
-  $radix_progress = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix-progress.js';
+  $radix_progress = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix.progress.js';
   if (!empty($javascript[$progress]) && empty($javascript[$radix_progress])) {
     $javascript[$radix_progress] = array_merge(
       drupal_js_defaults(), array('group' => JS_THEME, 'data' => $radix_progress));
