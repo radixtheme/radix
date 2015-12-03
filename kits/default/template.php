308025e81ac3e89ad8c111afb4fa540a2b7cd1f2
Arshad Chummun <arshad@chummun.com>
2015-12-03 20:39:09 +0400

    Add dynamic require_once for template.php

--- kits/default/template.php
+++ kits/default/template.php
@@ -4,17 +4,11 @@
  * Theme functions
  */
 
-require_once dirname(__FILE__) . '/includes/theme.inc';
-require_once dirname(__FILE__) . '/includes/structure.inc';
-require_once dirname(__FILE__) . '/includes/field.inc';
-require_once dirname(__FILE__) . '/includes/comment.inc';
-require_once dirname(__FILE__) . '/includes/form.inc';
-require_once dirname(__FILE__) . '/includes/menu.inc';
-require_once dirname(__FILE__) . '/includes/node.inc';
-require_once dirname(__FILE__) . '/includes/panel.inc';
-require_once dirname(__FILE__) . '/includes/block.inc';
-require_once dirname(__FILE__) . '/includes/user.inc';
-require_once dirname(__FILE__) . '/includes/view.inc';
+// Include all files from the includes directory.
+$includes_path = dirname(__FILE__) . '/includes/*.inc';
+foreach (glob($includes_path) as $filename) {
+  require_once dirname(__FILE__) . '/includes/' . basename($filename);
+}
 
 /**
  * Implements template_preprocess_page().
