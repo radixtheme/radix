66cdb425b96a0d3c0129410280e1f0990d980299
Arshad Chummun <arshad@chummun.com>
2018-06-24 21:35:44 +0400

    Initial commit for Bootstrap 4
    Upgraded Radix and the default kit to Bootstrap 4. Radix 8.x-4.x is now dependent on the components module.

---
+++ webpack.mix.js
@@ -0,0 +1,38 @@
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your application. See https://github.com/JeffreyWay/laravel-mix.
+ |
+ */
+const mix = require('laravel-mix');
+const glob = require('glob');
+
+/*
+ |--------------------------------------------------------------------------
+ | Configuration
+ |--------------------------------------------------------------------------
+ */
+mix
+  .setPublicPath('assets')
+  .disableNotifications();
+
+/*
+ |--------------------------------------------------------------------------
+ | Browsersync
+ |--------------------------------------------------------------------------
+ */
+mix.browserSync({
+  proxy: 'drupal.local',
+  files: ['assets/js/**/*.js', 'assets/css/**/*.css'],
+  stream: true,
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | SASS
+ |--------------------------------------------------------------------------
+ */
+mix.sass('src/sass/radix.style.scss', 'css');
