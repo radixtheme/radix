1abe1880c88aaf416ae911f86b98ccbccf7736cf
ckng <ckng@16307.no-reply.drupal.org>
2021-04-27 17:07:02 +0800

    Issue #3193624 by ckng: customizable Bootstrap JS

--- src/kits/default/src/js/default.script.js
+++ src/kits/default/src/js/default.script.js
@@ -1,14 +1,23 @@
-import 'popper.js';
-import 'bootstrap';
+import './_bootstrap.js';
 
-(function () {
+(function ($, Drupal) {
 
   'use strict';
 
-  Drupal.behaviors.helloWorld = {
-    attach: function (context) {
-      console.log('Hello World');
-    }
-  };
+  // // Enable Bootstrap Popover sitewide.
+  // // Popovers are opt-in for performance reasons.
+  // Drupal.behaviors.bsPopover = {
+  //   attach: function (context) {
+  //     $('[data-bs-toggle="popover"]').popover();
+  //   }
+  // };
+
+  // // Enable Bootstrap Toast sitewide.
+  // // Toasts are opt-in for performance reasons.
+  // Drupal.behaviors.bsToast = {
+  //   attach: function (context) {
+  //     $('.toast').toast('show');
+  //   }
+  // };
 
 })(jQuery, Drupal);
